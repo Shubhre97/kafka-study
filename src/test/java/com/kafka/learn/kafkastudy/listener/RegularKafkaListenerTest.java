d2fa9fb03a2b84e79e9f46340e16c585d4a61fe0
Shubhre97 <121853223+Shubhre97@users.noreply.github.com>
2025-12-26 22:44:39 +0530

    Refactor RegularKafkaListenerTest for dependency injection

--- src/test/java/com/kafka/learn/kafkastudy/listener/RegularKafkaListenerTest.java
+++ src/test/java/com/kafka/learn/kafkastudy/listener/RegularKafkaListenerTest.java
@@ -1,12 +1,21 @@
 package com.kafka.learn.kafkastudy.listener;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegularKafkaListenerTest {
+    @InjectMocks
+    private RegularKafkaListener regularKafkaListener;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
     @Test
     void testListenerInstantiation() {
-        RegularKafkaListener listener = new RegularKafkaListener();
-        assertNotNull(listener, "Listener should be instantiated");
+        assertNotNull(regularKafkaListener, "Listener should be instantiated");
     }
 }
